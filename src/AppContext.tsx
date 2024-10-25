b6bd1d0aa06b9e9fd0bbd211001e907147acd332
cdtharsh <harshalkenjale2@gmail.com>
2024-10-25 15:14:57 +0530

    Update AppContext.tsx

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,10 +1,37 @@
-import { createContext } from "react";
+// AppContext.tsx
 
-export const DefaultContext = {
+import { createContext, useContext } from "react";
+
+// Define the interface for the context
+interface AppContextType {
+  questions: any[];
+  correctAnswerCount: number;
+  incorrectAnswerCount: number;
+  totalQuestionCount: number;
+  setQuestions: (val: any[]) => void;
+  addOneToCorrectAnswerCount: () => void;
+  addOneToIncorrectAnswerCount: (val: any) => void;
+  setTotalQuestionCount: (val: number) => void;
+  resetContext: () => void;
+}
+
+// Define the default context values
+export const DefaultContext: AppContextType = {
   questions: [],
   correctAnswerCount: 0,
   incorrectAnswerCount: 0,
   totalQuestionCount: 0,
+  setQuestions: () => {},
+  addOneToCorrectAnswerCount: () => {},
+  addOneToIncorrectAnswerCount: () => {},
+  setTotalQuestionCount: () => {},
+  resetContext: () => {},
 };
 
-export const AppContext = createContext(DefaultContext);
+// Create the context with the default values
+export const AppContext = createContext<AppContextType>(DefaultContext);
+
+// Custom hook to use the AppContext
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
