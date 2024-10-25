1cbcc7865b1fcb117efdbabd043f5a6d9057909b
cdtharsh <harshalkenjale2@gmail.com>
2024-10-25 11:16:27 +0530

    Added Logo to Home Screen

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,7 +1,12 @@
 import "./Home.css";
+import logo from "../../../assets/logo.svg";
 
 function Home() {
-  return <div className="wrap_home_screen"></div>;
+  return (
+    <div className="wrap_home_screen">
+      <img src={logo} alt="logo" className="home_screen_logo"></img>
+    </div>
+  );
 }
 
 export default Home;
