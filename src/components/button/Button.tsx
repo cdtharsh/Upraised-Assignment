3aea272dbb584689602e89df791b109434a7b70d
cdtharsh <harshalkenjale2@gmail.com>
2024-10-25 11:47:36 +0530

    Added Button Component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+interface ButtonInterface {
+  text: string;
+  action: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  className?: string;
+  icon?: string;
+  disabled?: boolean;
+}
+
+function Button({
+  text,
+  action,
+  className = "",
+  icon,
+  disabled = false,
+}: ButtonInterface) {
+  return (
+    <button
+      className={`button_primary ${className}`}
+      onClick={action}
+      disabled={disabled}
+    >
+      {text}
+      {icon && (
+        <span>
+          <img src={icon} alt="button-icon" className="button_icon" />
+        </span>
+      )}
+    </button>
+  );
+}
+
+export default Button;
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,7 +1,13 @@
 import "./Home.css";
 import logo from "../../assets/logo.svg";
+import Button from "../../components/button/Button";
 
 function Home() {
+  const handleStartClick = () => {
+    // Define the action to be taken when the button is clicked
+    console.log("Start button clicked!");
+    // You can add more logic here, such as navigating to another page
+  };
   return (
     <div className="wrap_home_screen">
       <img src={logo} alt="logo" className="home_screen_logo"></img>
@@ -9,7 +15,11 @@ function Home() {
         <h2 className="home_screen_title">Quiz</h2>
       </div>
 
-      <button className="home_screen_start" title="Start"></button>
+      <Button
+        className="home_screen_start"
+        text="Start"
+        action={handleStartClick}
+      ></Button>
     </div>
   );
 }
