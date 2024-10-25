f6eae6a4280a1fc7c6e7d4d287bcd78eecf5696f
cdtharsh <harshalkenjale2@gmail.com>
2024-10-25 11:21:14 +0530

    Added Home Screen Body

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -5,6 +5,9 @@ function Home() {
   return (
     <div className="wrap_home_screen">
       <img src={logo} alt="logo" className="home_screen_logo"></img>
+      <div className="home_screen_body">
+        <h2 className="home_screen_title">Quiz</h2>
+      </div>
     </div>
   );
 }
