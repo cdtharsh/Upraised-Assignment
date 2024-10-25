9293d1d7ad4cc6b85f972bf84006d1e4710fe041
cdtharsh <harshalkenjale2@gmail.com>
2024-10-25 16:13:25 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+// App.tsx
+import { Routes, Route } from "react-router-dom";
 import Home from "./screens/home/Home";
+import Question from "./screens/question/Question";
+import Result from "./screens/result/Result";
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/question/:id" element={<Question />} />
+      <Route path="/result" element={<Result />} />
+    </Routes>
+  );
 }
 
 export default App;
