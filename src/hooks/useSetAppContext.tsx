1fc54b5bf8169453f754f8c9cdd55695bc5e70f5
cdtharsh <harshalkenjale2@gmail.com>
2024-10-25 14:33:21 +0530

    Added Hooks

---
+++ src/hooks/useSetAppContext.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { getInitialContextValues } from "../services/getInitialContextValues";
+
+export function useSetAppContext() {
+  const [context, setContext] = useState(getInitialContextValues);
+
+  return { context, setContext };
+}
